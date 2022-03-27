a75804309bf51aa5f96456c31601efd75c2ba8a2
ShivamGithub120999 <65438973+ShivamGithub120999@users.noreply.github.com>
2022-03-27 18:20:48 +0530

    First Implementation on React Redux Contact App

---
+++ src/components/AddContact/index.js
@@ -0,0 +1,195 @@
+/* import React, { useState } from "react";
+import { connect } from "react-redux";
+import { useHistory } from "react-router";
+import { toast } from "react-toastify";
+
+const AddPost = ({ contacts, addContact }) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+
+  const history = useHistory();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const checkContactEmailExists = contacts.filter((contact) =>
+      contact.email === email ? contact : null
+    );
+    const checkContactPhoneExists = contacts.filter((contact) =>
+      contact.phone === phone ? contact : null
+    );
+
+    if (!email || !name || !phone) {
+      return toast.warning("Please fill in all fields!!");
+    }
+    if (checkContactEmailExists.length > 0) {
+      return toast.error("This email already exists!!");
+    }
+    if (checkContactPhoneExists.length > 0) {
+      return toast.error("This phone number already exists!!");
+    }
+
+    const data = {
+      id: contacts.length > 0 ? contacts[contacts.length - 1].id + 1 : 0,
+      email,
+      name,
+      phone,
+    };
+
+    addContact(data);
+    toast.success("Contact added successfully!!");
+    history.push("/");
+  };
+
+  return (
+    <div className="container-fluid">
+      <h1 className="text-center text-dark py-3 display-2">Add Post</h1>
+      <div className="row">
+        <div className="col-md-6 p-5 mx-auto shadow">
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <input
+                className="form-control"
+                type="text"
+                placeholder="Full name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                className="form-control"
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                className="form-control"
+                type="number"
+                placeholder="Phone"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                className="btn btn-block btn-dark"
+                type="submit"
+                value="Add Student"
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  contacts: state,
+});
+const mapDispatchToProps = (dispatch) => ({
+  addContact: (data) => {
+    dispatch({ type: "ADD_CONTACT", payload: data });
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(AddPost);
+ */
+
+import React, { useState } from "react";
+import {useSelector,useDispatch} from "react-redux";
+import { useHistory } from "react-router";
+import { toast } from "react-toastify";
+const AddContact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+
+  const contacts = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  const history = useHistory();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const checkEmail = contacts.find(contact => contact.email === email && email);
+
+    const checkNumber = contacts.find(contact => contact.phone === parseInt(phone) && parseInt(phone));
+    
+    if(!email || !phone || !name){
+      return toast.warning("Please fill in all fields!");
+    }
+
+    if(checkEmail){
+      return toast.error("This email already Exists!");
+    }
+
+    if(checkNumber){ 
+      return toast.error("This Phone Number already Exists!");
+    }
+
+    const data = {
+      id: contacts.length > 0 ? contacts[contacts.length - 1].id + 1 : 0,
+      email,
+      name,
+      phone,
+    };
+
+    dispatch({type:"ADD_CONTACT",payload:data});
+    toast.success("Student added successfully!");
+    history.push("/");
+  };
+
+  return (
+    <div className="container">
+      <h1 className="display-3 my-5 text-center">Contact Add Contact</h1>
+      <div className="row d-flex flex-column">
+        <div className="col-md-6 shadow mx-auto p-5">
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <input
+                className="form-control"
+                type="text"
+                placeholder="Full Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                className="form-control"
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                className="form-control"
+                type="number"
+                placeholder="Phone"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                className="btn btn-block btn-dark"
+                type="submit"
+                value="Add Student"
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AddContact;
